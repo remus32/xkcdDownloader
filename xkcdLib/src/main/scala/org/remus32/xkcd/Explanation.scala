c3a2a3c155ec05ee476d85d812e20a1beea35df3
remus32 <matyas2303@gmail.com>
2016-01-31 18:25:06 +0100

    Add Explanations

--- xkcdLib/src/main/scala/org/remus32/xkcd/Explanation.scala
+++ xkcdLib/src/main/scala/org/remus32/xkcd/Explanation.scala
@@ -1,18 +1,14 @@
 package org.remus32.xkcd
 
-import java.net.URL
-
 import com.typesafe.scalalogging.LazyLogging
 
 /**
   * Created by remus32 on 31/01/16.
   */
-class Explanation(url: URL) extends LazyLogging {
-
+class Explanation(val comic: Comic) extends LazyLogging {
+  val apiUrl = Util.explanationApiUrl(comic.id)
 }
 
 object Explanation extends LazyLogging {
-  def apply(id: Comic): Explanation = apply(id.id)
-
-  private def apply(id: Int): Explanation = new Explanation(Util.explanationApiUrl(id))
+  def apply(id: Comic): Explanation = new Explanation(id)
 }
