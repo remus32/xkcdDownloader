79345625fe694a1648aebcd78220c8413c518ff1
remus32 <matyas2303@gmail.com>
2016-01-31 18:19:42 +0100

    Update tests

--- xkcdLib/src/test/scala/org/remus32/xkcd/test/ComicTest.scala
+++ xkcdLib/src/test/scala/org/remus32/xkcd/test/ComicTest.scala
@@ -1,39 +1,91 @@
 package org.remus32.xkcd.test
 
-import org.remus32.xkcd.{BadComicIdException, Comic, ComicNotFoundException, Util}
+import java.io.FileInputStream
+import java.net.URL
+
+import org.apache.commons.io.IOUtils
+import org.remus32.xkcd._
+import org.remus32.xkcd.caches.File
 import org.scalatest.FlatSpec
 
 class ComicTest extends FlatSpec {
 
-  "Comic resolver" should "throw BadComicIdException if stupid string is given" in {
-    System.setProperty("xkcd.cache", "memory")
+  def init() = {
+    System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "warn")
+    System.setProperty("xkcd.cache", "file")
     Util.init()
+  }
+
+  "Cache controller" should "select File wrapper if no property given" in {
+    assert(Cache.cache.isInstanceOf[File])
+  }
+
+  "Cache controller" should "select File wrapper if empty property given" in {
+    System.setProperty("xkcd.cache", "")
+    assert(Cache.cache.isInstanceOf[File])
+  }
+
+  "Comic resolver" should "throw BadComicIdException if stupid string is given" in {
+    init()
     intercept[BadComicIdException] {
       Comic.resolveComic("fafpdmpaojmdf")
     }
   }
 
   "Comic factory" should "throw ComicNotFoundException if non-existent comic id is given" in {
-    System.setProperty("xkcd.cache", "memory")
-    Util.init()
+    init()
     intercept[ComicNotFoundException] {
       Comic(Comic.latest.id * 2)
     }
   }
 
   it should "make Comic404 if 404 id given" in {
-    System.setProperty("xkcd.cache", "memory")
-    Util.init()
+    init()
     val comic = Comic(404)
     assert(comic.data.num == 404)
     assert(comic.image.toFile().isFile)
   }
 
   "Comic explanation" should "exist" in {
-    System.setProperty("xkcd.cache", "memory")
-    Util.init()
+    init()
     val comic = Comic(723)
     assert(comic.explain.toURI.isAbsolute)
   }
 
+  "Reference" should "work" in {
+    init()
+    val ref = Cache.cache.make("test0")
+    ref.load(new URL(Util.xkcdBase + "732/info.0.json"))
+    assert(ref.read == Comic(732).metaRef.read())
+    assert(ref.toFile().toString != Comic(732).metaRef.toFile().toString)
+    assert(IOUtils.toString(new FileInputStream(ref.toFile())) == IOUtils.toString(new FileInputStream(Comic(732).metaRef.toFile())))
+  }
+
+  "List of all comics" should "contain all comics" in {
+    init()
+    assert(Comic.comicList.length == Comic.latest.id)
+  }
+
+  "Comic name resolver" should "resolve Ints into their Int representation" in {
+    assert(Comic.resolveComic("255") == 255)
+  }
+
+  it should "resolve last to latest comic id" in {
+    assert(Comic.resolveComic("last") == Comic.latest.id)
+  }
+
+  it should "resolve random into valid random comic ids" in {
+    for (x <- 1 to 1000) {
+      assert(0 < Comic.resolveComic("random"))
+      assert(Comic.resolveComic("random") < (Comic.latest.id + 1))
+    }
+  }
+
+  "Random comic number generator" should "return existing comic ids" in {
+    for (x <- 1 to 1000) {
+      val d = Comic.randomComicId()
+      assert(0 < d)
+      assert(d < (Comic.latest.id + 1))
+    }
+  }
 }
---
+++ xkcdLib/src/test/scala/org/remus32/xkcd/test/UtilTest.scala
@@ -0,0 +1,100 @@
+package org.remus32.xkcd.test
+
+import java.io
+import java.io.{File, FileInputStream}
+
+import org.apache.commons.io.IOUtils
+import org.remus32.xkcd.Util._
+import org.remus32.xkcd.{Comic, Util}
+import org.scalatest.FlatSpec
+
+class UtilTest extends FlatSpec {
+  def init() = {
+  }
+
+  case class Pattern(int: Int, string: String)
+
+  "Gson in util" should "parse json" in {
+    init()
+    val a = new Pattern(2, "hello")
+    val b = "{int: 2, string: \"hello\"}"
+    assert(a.int == gson.fromJson(b, classOf[Pattern]).int)
+    assert(a.string == gson.fromJson(b, classOf[Pattern]).string)
+  }
+
+  "Gson in util" should "make json" in {
+    init()
+    val a = new Pattern(2, "hello")
+    val b = "{int: 2, string: \"hello\"}"
+    assert(
+      gson.fromJson(b, classOf[Pattern]) == gson.fromJson(gson.toJson(a), classOf[Pattern])
+    )
+  }
+
+  "Downloader" should "download files from url" in {
+    init()
+    val res = getClass.getResource("/test0")
+    val out = new File(cache(), "test0")
+    val compare = "hello world"
+    download(res, out)
+    assert(compare == IOUtils.toString(new FileInputStream(out)))
+  }
+
+  "Deleteonexit" should "be false if property is not defined" in {
+    init()
+    //noinspection SimplifyBoolean
+    assert(deleteOnExit() == false)
+  }
+
+  it should "be true if property set to true" in {
+    init()
+    System.setProperty("xkcd.deleteonexit", "true")
+    //noinspection SimplifyBoolean
+    assert(deleteOnExit() == true)
+  }
+
+  "Init method" should "create directory for cache" in {
+    init()
+    Util.init()
+    assert(cache().isDirectory)
+  }
+
+  it should "create directory for output" in {
+    init()
+    Util.init()
+    assert(out().isDirectory)
+  }
+
+  it should "load default cache" in {
+    init()
+    Util.init()
+    val int: Int = cache.listFiles.length
+    assert(int > Comic.latest.id || int == Comic.latest.id)
+  }
+
+  "Cache method" should "return cwd+/xkcdCache if no property given" in {
+    assert(cache.getAbsolutePath == new io.File(System.getProperty("user.dir") + "/xkcdCache").getAbsolutePath)
+  }
+
+  it should "be property value if given" in {
+    val before = cache().getAbsolutePath
+    System.setProperty("xkcd.cacheDir", "/home")
+    assert(cache().getAbsolutePath == "/home")
+    System.setProperty("xkcd.cacheDir", before)
+  }
+
+  "Output method" should "return cwd+/xkcd if no property given" in {
+    assert(out.getAbsolutePath == new io.File(System.getProperty("user.dir") + "/xkcd").getAbsolutePath)
+  }
+
+  it should "be property value if given" in {
+    val before = out().getAbsolutePath
+    System.setProperty("xkcd.out", "/home")
+    assert(out().getAbsolutePath == "/home")
+    System.setProperty("xkcd.out", before)
+  }
+
+  "Explanation api url" should "work" in {
+    assert(explanationApiUrl(452).toURI.isAbsolute)
+  }
+}
