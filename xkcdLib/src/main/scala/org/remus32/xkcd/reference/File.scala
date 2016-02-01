36081a5997e4fac32d7313790e098e42cc880cf5
remus32 <matyas2303@gmail.com>
2016-02-01 10:45:09 +0100

    Update FIle  reference

--- xkcdLib/src/main/scala/org/remus32/xkcd/reference/File.scala
+++ xkcdLib/src/main/scala/org/remus32/xkcd/reference/File.scala
@@ -14,15 +14,31 @@ import org.remus32.xkcd.{Reference, Util}
 class File(val name: String) extends Reference with LazyLogging {
   val file: io.File = new io.File(cache(), name)
 
-  def copyTo(to: io.File): Unit = FileUtils.copyFile(file, to)
+  def copyTo(to: io.File): Unit = if (exist()) FileUtils.copyFile(file, to)
 
-  def clean(): Unit = file.delete()
+  def clean(): Unit = if (exist()) file.delete()
 
   def exist(): Boolean = file.isFile
 
-  def read(): String = IOUtils.toString(new FileReader(file))
+  def read(): String = {
+    if (exist())
+      IOUtils.toString(new FileReader(file))
+    else {
+      val clazz = Thread.currentThread().getStackTrace()(1).getClassName()
+      val method = Thread.currentThread().getStackTrace()(1).getMethodName()
+      logger.error(s"$clazz.$method was trying to read empty reference!")
+      ""
+    }
+  }
 
-  def load(from: URL): Unit = if (!(file.isFile && file.length() > 128L)) Util.download(from, file)
+  def load(from: URL): Unit = if (!(file.isFile && file.length() > 128L)) {
+    create()
+    Util.download(from, file)
+  }
+
+  def create() = {
+    file.createNewFile()
+  }
 
   def toFile: io.File = file
 }
